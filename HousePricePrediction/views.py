de6642f0d74c991b46b39266c5c8bc45a1ed15c3
Krish Sangani <rocket.20.launcher18@gmail.com>
2026-03-12 12:34:20 +0530

    Updated

--- HousePricePrediction/views.py
+++ HousePricePrediction/views.py
@@ -39,7 +39,7 @@ def result(request):
         city = request.GET['J5']         # City (string)
         furnishing = request.GET['J6']   # Furnishing Status (string)
 
-        #Validate Purchase Budget based on BHK
+        # Validate Purchase Budget based on BHK
         bhk_min_budget = {
             1: 1500000, 1.5: 2200000, 2: 2800000, 2.5: 3400000,
             3: 4000000, 3.5: 4600000, 4: 5200000, 4.5: 5800000, 5: 6900000
