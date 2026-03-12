bc714b65b140296e4e59bfa1763279bc9617a74f
Krish Sangani <rocket.20.launcher18@gmail.com>
2026-03-12 15:04:25 +0530

    Updated

--- train_model.py
+++ train_model.py
@@ -20,7 +20,6 @@ X = df.drop(target, axis=1)
 y = df[target]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 5. Train an Advanced Model (Random Forest for 95%+ Accuracy potential)
 model = RandomForestRegressor(n_estimators=200, max_depth=15, random_state=42)
 model.fit(X_train, y_train)
 
