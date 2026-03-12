69b495ec072530fbb6cb92d1002532b563e0462b
Krish Sangani <rocket.20.launcher18@gmail.com>
2026-03-12 15:01:46 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -6,9 +6,6 @@ from sklearn.metrics import r2_score
 import joblib
 
 data = pd.read_csv("./india_housing_prices.csv")
-
-# 2. Select matching features based on your CSV image
-# We will use: City, BHK, Size_in_SqFt, Age_of_Property, Furnished_Status
 features = ['City', 'BHK', 'Size_in_SqFt', 'Age_of_Property', 'Furnished_Status']
 target = 'Price_in_Lakhs'
 
