0ac2947d18be75e8f969b0b261a2148e87222437
Krish Sangani <rocket.20.launcher18@gmail.com>
2026-03-18 18:38:14 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -4,6 +4,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 import joblib
+
 data = pd.read_csv("./india_housing_prices.csv")
 features = ['City', 'BHK', 'Size_in_SqFt', 'Age_of_Property', 'Furnished_Status']
 target = 'Price_in_Lakhs'
