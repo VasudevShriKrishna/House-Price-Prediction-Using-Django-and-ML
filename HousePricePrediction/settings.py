9c3c81cf951bc63ea0ef35834f4f17c218b32345
Krish Sangani <rocket.20.launcher18@gmail.com>
2026-07-11 20:16:01 +0530

    Update settings.py

--- HousePricePrediction/settings.py
+++ HousePricePrediction/settings.py
@@ -8,14 +8,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'USE YOUR GENERATED DJANGO PROJECT KEY'
+SECRET_KEY = 'django-insecure-0d7js^q1ldd$gom@hyzx=wq0vg@p9^jfv9eobg&bf7q&4l*xy1'
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
 
 # Application definition
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
