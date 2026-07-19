150174be8d713e1fbe4752a03cc9076e2ea561bc
Krish Sangani <rocket.20.launcher18@gmail.com>
2026-07-19 19:23:38 +0530

    Update urls.py

--- HousePricePrediction/urls.py
+++ HousePricePrediction/urls.py
@@ -1,23 +1,11 @@
-"""HousePricePrediction URL Configuration
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from . import  views
+from django.urls import path 
+from HousePricePrediction import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home),
-    path('predict/', views.predict), # prediction route
-    path('result/', views.result),
-    path('history/', views.prediction_history), # must needed this route to ccheck history
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("predict/", views.predict, name="predict"),
+    path("result/", views.result, name="result"),
+    path("history/", views.prediction_history, name="prediction_history"),
 ]
